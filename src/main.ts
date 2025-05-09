2d3aef5753e87c2f21a1aa065aa3ec481bd1349b
scotch <insafgaraev47@gmail.com>
2025-05-09 20:04:26 +0300

    fix:small fies

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from './pipes/validation.pipe';
 
 async function start() {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
 
   const configSwagger = new DocumentBuilder()
