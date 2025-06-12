5412c8abd0bb990ec9cc92d3f837fcb9c04e6642
scotch <insafgaraev47@gmail.com>
2025-06-12 16:05:20 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,24 @@ async function start() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
 
+  // 👉 добавляем глобальный префикс
+  app.setGlobalPrefix('api');
+
+  // Swagger на /api/docs
   const configSwagger = new DocumentBuilder()
     .setTitle('Personal Bookeeping API')
     .setDescription('Documentation of REST API')
     .setVersion('1.0.0')
     .addTag('Scotch')
-    .addBearerAuth() // Добавляем поддержку Bearer-токена
+    .addBearerAuth()
     .build();
   const documentation = SwaggerModule.createDocument(app, configSwagger);
-  SwaggerModule.setup('/api/docs', app, documentation);
+  SwaggerModule.setup('api/docs', app, documentation); // ← без начального "/"
 
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () =>
-    console.log(
-      `server works on ${process.env.PORT} in ${process.env.NODE_ENV} mode`,
-    ),
+    console.log(`server works on ${PORT} in ${process.env.NODE_ENV} mode`),
   );
 }
 
